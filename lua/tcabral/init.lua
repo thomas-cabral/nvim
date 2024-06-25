40190e776bccce8345002125ee630bec3d42dcb9
tcabral <thomas@thomascabral.tech>
2024-06-25 15:02:28 -0400

    Fixed auto import of packer

--- lua/tcabral/init.lua
+++ lua/tcabral/init.lua
@@ -1,8 +1,9 @@
 require("tcabral.set")
 require("tcabral.remap")
+require("tcabral.packer")
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local ThePrimeagenGroup = augroup('tcabral', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
@@ -23,7 +24,7 @@ autocmd('TextYankPost', {
 })
 
 autocmd({"BufWritePre"}, {
-    group = ThePrimeagenGroup,
+    group = tcabral,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
