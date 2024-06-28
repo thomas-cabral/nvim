075751387698e4b95ce722305c2efbc0baa0d5cb
tcabral <thomas@thomascabral.tech>
2024-06-28 19:43:32 -0400

    adding go nil check remap

--- lua/tcabral/remap.lua
+++ lua/tcabral/remap.lua
@@ -45,6 +45,8 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
