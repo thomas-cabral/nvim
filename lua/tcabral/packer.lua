3e10391f88d86514983d8c42f1af942a23c485b5
thomas cabral <thomas@thomascabral.tech>
2024-07-09 18:30:41 -0400

    added scala lsp

--- lua/tcabral/packer.lua
+++ lua/tcabral/packer.lua
@@ -52,7 +52,30 @@ return require('packer').startup(function(use)
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
   use("nvim-treesitter/nvim-treesitter-context");
+  use{"scalameta/nvim-metals",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      ft = { "scala", "sbt", "java" },
+      opts = function()
+        local metals_config = require("metals").bare_config()
+        metals_config.on_attach = function(client, bufnr)
+          -- your on_attach function
+        end
 
+        return metals_config
+      end,
+      config = function(self, metals_config)
+        local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = self.ft,
+          callback = function()
+            require("metals").initialize_or_attach(metals_config)
+          end,
+          group = nvim_metals_group,
+        })
+      end
+    }
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v1.x',
